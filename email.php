3433c72276d3a66a56dedf63475949489302e370
Sh <nazriev.shavqatjon@gmail.com>
2022-12-19 16:53:50 +0500

    Testing email

--- email.php
+++ email.php
@@ -3,18 +3,21 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 
 if ($name && $phone) {
-  $to = 'info@asiapharma.tj';
-  $subject = 'Новое сообщение из сайта asiapharm.tj';
+  $to = 'info@orienpharm.tj';
+  $subject = 'Новое сообщение из сайта orienpharm.tj';
   $message = "Новый запрос о бесплатной консультации. Имя: $name. Телефон: $phone";
 
-  $headers = 'From: test@demo.asiapharm.tj' . "\r\n" .
-    'Reply-To: test@demo.asiapharm.tj' . "\r\n" .
+  $headers = 'From: web@demo.orienpharm.tj' . "\r\n" .
+    'Reply-To: web@demo.orienpharm.tj' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
   if (mail($to, $subject, $message, $headers)) {
-    header("Location: http://asiapharm.tj/");
+    header("Location: https://orienpharm.tj/");
     die();
   } else {
     echo "Сервис временно не работает!";
   }
+} else {
+  header("Location: https://orienpharm.tj/");
+  die();
 }
